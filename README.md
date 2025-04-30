fcc0fd6db0bcb74c740f71f63d836777e86ddb3e
TWintersww <134812478+TWintersww@users.noreply.github.com>
2025-04-29 21:28:02 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 5 - Starter
+
+Name: Evan Wu
+
+Check Your Understanding:
+
+1. No. I believe a message feature of a messaging application has too many working parts, from the frontend event of one user that makes the request, to the encryption protocol, server's receival of the message, decryption, and then business logic that ultimately renders the message to the frontend of the receiver. There are too many interconnected parts for a unit test, which is supposed to be modular.
+
+2. Yes. We could implement a bunch of tests that try calling this specific function to make sure it works only with certain inputs, works for edge cases, and does exactly what it is supposed to do so we can be sure down the line that when something breaks, it likely isn't this function.
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,45 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const hornSelector = document.getElementById("horn-select")
+  //console.log(hornSelector)
+  const audioElement = document.querySelector(`audio[class="hidden"]`)
+  audioElement.volume = 0.5
+  
+  hornSelector.addEventListener("change", function() {
+    const hornImage = document.querySelector(`img[alt="No image selected"]`)
+    hornImage.src = `assets/images/${hornSelector.value}.svg`
+    audioElement.src = `assets/audio/${hornSelector.value}.mp3`
+  })
+
+  const playSoundButton = document.querySelector("button")
+  //console.log(playSoundButton)
+  playSoundButton.addEventListener("click", function() {
+    audioElement.play()
+
+    if (audioElement.src.includes('party')) {
+      //console.log('shoot confetti')
+      const jsConfetti = new JSConfetti()
+      jsConfetti.addConfetti()
+    }
+  })
+
+  const volumeBar = document.getElementById("volume")
+  //console.log(volumeBar)
+  volumeBar.addEventListener("input", function () {
+    audioElement.volume = (volumeBar.value) / 100
+    const volumeIcon = document.querySelector(`img[alt="Volume level 2"]`)
+    if (volumeBar.value == 0) {
+      volumeIcon.src = `assets/icons/volume-level-0.svg`
+    }
+    else if (volumeBar.value < 33) {
+      volumeIcon.src = `assets/icons/volume-level-1.svg`
+    }
+    else if (volumeBar.value < 67) {
+      volumeIcon.src = `assets/icons/volume-level-2.svg`
+    }
+    else {
+      volumeIcon.src = `assets/icons/volume-level-3.svg`
+    }
+  })
 }
